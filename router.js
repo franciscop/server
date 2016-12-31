db78984db68af75cda1c928b722aa37906dd32e4
Francisco Presencia Fandos <developerfrancisco@hotmail.com>
2016-12-31 17:53:03 +0100

    Updated web and added join

--- router.js
+++ router.js
@@ -1,18 +1,19 @@
 let express = require('express');
 let loadware = require('loadware');
 
-module.exports = new Proxy({}, {
-  get: (orig, key) => (...args) => {
-    let router = express.Router();
-    router[key](...args);
-    return router;
-  }
-});
-
-module.exports.join = (...middles) => {
+let join = (...middles) => {
   let router = express.Router();
   loadware(middles).forEach(middle => {
     router.use(middle);
   });
   return router;
 }
+
+module.exports = new Proxy({}, {
+  get: (orig, key) => (...args) => {
+    if (key === 'join') return join(...args);
+    let router = express.Router();
+    router[key](...args);
+    return router;
+  }
+});
