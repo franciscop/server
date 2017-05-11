7b24c4765d36896ebc4ad1d3ec1a5a504123db9d
Francisco Presencia Fandos <developerfrancisco@hotmail.com>
2017-05-11 20:55:26 +0200

    Where the hell does that come from?

--- plugins/connect/init.js
+++ plugins/connect/init.js
@@ -11,7 +11,7 @@ module.exports = ctx => {
 
   // Inherit
   options.public = options.public || ctx.options.public;
-  console.log('Public:', options.public, process.cwd(), path.join(process.cwd(), options.public));
+  console.log('Public:', process.env.PUBLIC, process.env.public, options.public, process.cwd(), path.join(process.cwd(), options.public));
   if (!path.isAbsolute(options.public)) {
     options.public = path.join(process.cwd(), options.public);
   }
