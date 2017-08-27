ce8eff2450f9f7dfef2a60a2605f04fd1942987d
Francisco Presencia <public@francisco.io>
2017-08-27 17:05:38 +0200

    Native errors remain native so far

--- plugins/core/errors.js
+++
@@ -1,9 +0,0 @@
-const errors = require('human-error')({
-  url: key => `https://serverjs.io/errors#${key.toLowerCase()}`
-});
-
-// errors.NotSoSecret = () => `
-//   Oh cmon! Don't use 'your-random-string-here' as your secret!
-// `;
-
-module.exports = errors;
