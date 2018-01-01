ce128236c8d38243cce85d2bf11f2487cc3c75c3
Francisco Presencia <public@francisco.io>
2018-01-01 05:26:32 +0100

    Fixed example

--- examples/websocket/index.js
+++ examples/websocket/index.js
@@ -3,10 +3,23 @@ const { get, socket } = server.router;
 const { render } = server.reply;
 
 server(
-  get('/', () => render('index.html')),
+  get('/', ctx => {
+    ctx.session.counter = ctx.session.counter || 0;
+    console.log(ctx.session.counter);
+    return render('index.html');
+  }),
   socket('connect', ctx => {
+
+    // Emit an event every second with +1 on the session
     setInterval(() => {
-      ctx.session.counter = (ctx.session.counter || 0) + 1;
+
+      // Increment the counter
+      ctx.session.counter++;
+
+      // For socket.io you need to manually save it
+      ctx.session.save();
+
+      // Send the value to the currently connected socket
       ctx.socket.emit('message', ctx.session.counter);
     }, 1000);
   })
