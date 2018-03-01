3d1e04728f4e0a47391c122ba28b8adbd3f175ec
Francisco Presencia <public@francisco.io>
2018-03-01 22:02:43 +0900

    Small test

--- test/examples/test-9.test.js
+++ test/examples/test-9.test.js
@@ -1,22 +1,13 @@
     /* test */
     const test = require('server/test');
     
-    // The middleware function that we want to test
-    // in this example, find a user in the list
-    const validTokens = ['t353459821389', 't547432523454', 't564352424223'];
-    const mid = ctx => {
-      if (validTokens.includes(ctx.query.token)) {
-        return 'Valid!';
-      }
-      return 'Invalid :(';
-    };
+    // The middleware function that we want to test. Just renders 'Hello world'
+    const mid = ctx => 'Hello world';
     
-    describe('auth', () => {
-      it('correctly handles admin or user emails', async () => {
-        const validRes = await test(mid).get('/?token=t353459821389');
-        expect(validRes.body).toBe('Valid!');
-    
-        const invalidRes = await test(mid).get('/?token=madeuptoken');
-        expect(invalidRes.body).toBe('Invalid :(');
+    describe('simple route', () => {
+      it('correctly returns a Hello world', async () => {
+        const res = await test(mid).get('/');
+        expect(res.body).toBe('Hello world');
+        expect(res.status).toBe(200);
       });
     });
