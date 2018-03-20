94554e33e6e6b9f1ac4d749059c4c5a087c24d3b
Francisco Presencia <public@francisco.io>
2018-03-20 19:46:14 +0900

    Simplified

--- examples/stream/index.js
+++ examples/stream/index.js
@@ -1,10 +1,9 @@
+const server = require('../../server');
 const fs = require('fs');
 const path = require('path');
 
 const img = path.resolve('../../test/logo.png');
 
-require('../../server')(ctx => {
-  return new Promise(resolve => {
-    fs.createReadStream(img).pipe(ctx.res).on('end', resolve);
-  });
-});
+server(ctx => new Promise(resolve => {
+  fs.createReadStream(img).pipe(ctx.res).on('end', resolve);
+}));
