6e444a7ad374674a31e9afc1ff3152a6f89ec12b
Francisco Presencia <public@francisco.io>
2020-04-01 01:49:22 +0200

    Updated streaming example

--- examples/stream/index.js
+++ examples/stream/index.js
@@ -1,9 +1,16 @@
-const server = require('../../server');
-const fs = require('fs');
-const path = require('path');
+const server = require("../../server");
+const fs = require("fs");
+const path = require("path");
 
-const img = path.resolve('../../test/logo.png');
+const img = path.resolve("../../test/logo.png");
+const stream = (read, write) =>
+  new Promise((resolve, reject) => {
+    read
+      .pipe(write)
+      .on("error", reject)
+      .on("end", resolve);
+  });
 
-server(ctx => new Promise(resolve => {
-  fs.createReadStream(img).pipe(ctx.res).on('end', resolve);
-}));
+server(ctx => {
+  return stream(fs.createReadStream(img), ctx.res);
+});
