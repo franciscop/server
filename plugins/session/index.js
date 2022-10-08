87506ed1e8d22592798484b74d57d102300e739b
Minty <kinostl@gmail.com>
2022-10-08 12:28:07 -0400

    Add ioredis

--- plugins/session/index.js
+++ plugins/session/index.js
@@ -1,9 +1,10 @@
-const modern = require('../../src/modern');
-const server = require('../../server');
-const session = require('express-session');
-server.session = session;
-const RedisStore = require('connect-redis')(server.session);
-let sessionMiddleware;
+const modern = require('../../src/modern')
+const server = require('../../server')
+const session = require('express-session')
+server.session = session
+const RedisStore = require('connect-redis')(server.session)
+let sessionMiddleware
+const Redis = require('ioredis')
 
 module.exports = {
   name: 'session',
@@ -34,18 +35,17 @@ module.exports = {
   },
   init: ctx => {
     if (!ctx.options.session.store && ctx.options.session.redis) {
-      ctx.options.session.store = new RedisStore({
-        url: ctx.options.session.redis
-      });
+      const redisClient = new Redis(ctx.options.session.redis)
+      ctx.options.session.store = new RedisStore({ client: redisClient })
     }
-    sessionMiddleware = session(ctx.options.session);
+    sessionMiddleware = session(ctx.options.session)
   },
   before: ctx => modern(sessionMiddleware)(ctx),
   launch: ctx => {
     // Return early if the Socket plugin is not enabled
-    if (!ctx.io || !ctx.io.use) return;
-    ctx.io.use(function(socket, next) {
-      sessionMiddleware(socket.request, socket.request.res || {}, next);
-    });
+    if (!ctx.io || !ctx.io.use) return
+    ctx.io.use(function (socket, next) {
+      sessionMiddleware(socket.request, socket.request.res || {}, next)
+    })
   }
-};
+}
